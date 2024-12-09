d4cf4cc9e1c6fa489aa5b920625a3f3bf51aaa45
nhlong112 <nhlong112@gmail.com>
2024-12-09 18:27:39 +0700

    xuly anh

--- train.py
+++ train.py
@@ -18,7 +18,7 @@ window.geometry('1600x900')
 dialog_title = 'Thoát'
 dialog_text = 'Bạn muốn thoát?'
 #answer = messagebox.askquestion(dialog_title, dialog_text)
- 
+
 #window.geometry('1280x720')
 window.configure(background='LightBlue4')
 
@@ -48,43 +48,43 @@ txt2.place(x=700, y=315)
 lbl3 = tk.Label(window, text="Thông báo : ",width=20  ,fg="white"  ,bg="gray25"  ,height=2 ,font=('times', 15, ' bold underline '))
 lbl3.place(x=400, y=400)
 
-message = tk.Label(window, text="" ,bg="gray25"  ,fg="white"  ,width=30  ,height=2, activebackground = "yellow" ,font=('times', 15, ' bold ')) 
+message = tk.Label(window, text="" ,bg="gray25"  ,fg="white"  ,width=30  ,height=2, activebackground = "yellow" ,font=('times', 15, ' bold '))
 message.place(x=700, y=400)
 
 lbl3 = tk.Label(window, text="Thông tin điểm danh : ",width=20  ,fg="white"  ,bg="gray25"  ,height=2 ,font=('times', 15, ' bold  underline'))
 lbl3.place(x=400, y=650)
 
 
-message2 = tk.Label(window, text="" ,fg="white"   ,bg="gray25",activeforeground = "green",width=30  ,height=2  ,font=('times', 15, ' bold ')) 
+message2 = tk.Label(window, text="" ,fg="white"   ,bg="gray25",activeforeground = "green",width=30  ,height=2  ,font=('times', 15, ' bold '))
 message2.place(x=700, y=650)
- 
+
 def clear():
-    txt.delete(0, 'end')    
+    txt.delete(0, 'end')
     res = ""
     message.configure(text= res)
 
 def clear2():
-    txt2.delete(0, 'end')    
+    txt2.delete(0, 'end')
     res = ""
-    message.configure(text= res)    
-    
+    message.configure(text= res)
+
 def is_number(s):
     try:
         float(s)
         return True
     except ValueError:
         pass
- 
+
     try:
         import unicodedata
         unicodedata.numeric(s)
         return True
     except (TypeError, ValueError):
         pass
- 
+
     return False
- 
-def TakeImages():        
+
+def TakeImages():
     Id=(txt.get())
     name=(txt2.get())
     if(is_number(Id) and name.isalpha()):
@@ -97,7 +97,7 @@ def TakeImages():
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             faces = detector.detectMultiScale(gray, 1.3, 5)
             for (x,y,w,h) in faces:
-                cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)        
+                cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
 
                 sampleNum=sampleNum+1
 
@@ -110,7 +110,7 @@ def TakeImages():
             elif sampleNum>100: #luu anh cho den khi dc 100 anh
                 break
         cam.release()
-        cv2.destroyAllWindows() 
+        cv2.destroyAllWindows()
         res = "Ảnh đã được lưu với ID : " + Id +" - Tên : "+ name
         row = [Id , name]
         with open('StudentDetails\StudentDetails.csv','a+') as csvFile:
@@ -125,7 +125,7 @@ def TakeImages():
         if (name.isalpha()):
             res = "Enter Numeric Id"
             message.configure(text=res)
-    
+
 def TrainImages():
     recognizer = cv2.face_LBPHFaceRecognizer.create()
     harcascadePath = "haarcascade_frontalface_default.xml"
@@ -138,9 +138,9 @@ def TrainImages():
 
 def getImagesAndLabels(path):
     #get the path of all the files in the folder
-    imagePaths=[os.path.join(path,f) for f in os.listdir(path)] 
+    imagePaths=[os.path.join(path,f) for f in os.listdir(path)]
     #print(imagePaths)
-    
+
     #create empth face list
     faces=[]
     #create empty ID list
@@ -155,50 +155,50 @@ def getImagesAndLabels(path):
         Id=int(os.path.split(imagePath)[-1].split(".")[1])
         # extract the face from the training image sample
         faces.append(imageNp)
-        Ids.append(Id)        
+        Ids.append(Id)
     return faces,Ids
 
 def TrackImages():
     recognizer = cv2.face.LBPHFaceRecognizer_create()#cv2.createLBPHFaceRecognizer()
     recognizer.read("TrainingImageLabel\Trainner.yml")
     harcascadePath = "haarcascade_frontalface_default.xml"
-    faceCascade = cv2.CascadeClassifier(harcascadePath);    
+    faceCascade = cv2.CascadeClassifier(harcascadePath);
     df=pd.read_csv("StudentDetails\StudentDetails.csv")
     cam = cv2.VideoCapture(0)
-    font = cv2.FONT_HERSHEY_SIMPLEX        
+    font = cv2.FONT_HERSHEY_SIMPLEX
     col_names =  ['Id','Name','Date','Time']
-    attendance = pd.DataFrame(columns = col_names)    
+    attendance = pd.DataFrame(columns = col_names)
     while True:
         ret, im =cam.read()
         gray=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
-        faces=faceCascade.detectMultiScale(gray, 1.2,5)    
+        faces=faceCascade.detectMultiScale(gray, 1.2,5)
         for(x,y,w,h) in faces:
             cv2.rectangle(im,(x,y),(x+w,y+h),(225,0,0),2)
-            Id, conf = recognizer.predict(gray[y:y+h,x:x+w])                                   
+            Id, conf = recognizer.predict(gray[y:y+h,x:x+w])
             if(conf < 50):
-                ts = time.time()      
+                ts = time.time()
                 date = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d')
                 timeStamp = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
                 aa=df.loc[df['Id'] == Id]['Name'].values
                 tt=str(Id)+"-"+aa
                 attendance.loc[len(attendance)] = [Id,aa,date,timeStamp]
-                
+
             else:
-                Id='Unknown'                
-                tt=str(Id)  
+                Id='Unknown'
+                tt=str(Id)
             if(conf > 75):
                 noOfFile=len(os.listdir("ImagesUnknown"))+1
-                cv2.imwrite("ImagesUnknown\Image"+str(noOfFile) + ".jpg", im[y:y+h,x:x+w])            
-            cv2.putText(im,str(tt),(x,y+h), font, 1,(255,255,255),2)        
-        attendance=attendance.drop_duplicates(subset=['Id'],keep='first')    
-        cv2.imshow('im',im) 
+                cv2.imwrite("ImagesUnknown\Image"+str(noOfFile) + ".jpg", im[y:y+h,x:x+w])
+            cv2.putText(im,str(tt),(x,y+h), font, 1,(255,255,255),2)
+        attendance=attendance.drop_duplicates(subset=['Id'],keep='first')
+        cv2.imshow('im',im)
         if (cv2.waitKey(1)==ord('q')):
             break
-    ts = time.time()      
+    ts = time.time()
     date = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d')
     timeStamp = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
     Hour,Minute,Second=timeStamp.split(":")
-    fileName="Attendance\Attendance_"+date+"_"+Hour+"-"+Minute+"-"+Second+".csv"
+    fileName="Attendance\Attendance_"+date+".csv"
     attendance.to_csv(fileName,index=False)
     cam.release()
     cv2.destroyAllWindows()
@@ -206,11 +206,11 @@ def TrackImages():
     res=attendance
     message2.configure(text= res)
 
-  
+
 clearButton = tk.Button(window, text="Xóa", command=clear  ,fg="steel blue"  ,bg="OliveDrab1"  ,width=20  ,height=2 ,activebackground = "white" ,font=('times', 15, ' bold '))
 clearButton.place(x=950, y=200)
 clearButton2 = tk.Button(window, text="Xóa", command=clear2  ,fg="steel blue"  ,bg="OliveDrab1"  ,width=20  ,height=2, activebackground = "white" ,font=('times', 15, ' bold '))
-clearButton2.place(x=950, y=300)    
+clearButton2.place(x=950, y=300)
 takeImg = tk.Button(window, text="Chụp ảnh", command=TakeImages  ,fg="steel blue"  ,bg="OliveDrab1"  ,width=20  ,height=3, activebackground = "white" ,font=('times', 15, ' bold '))
 takeImg.place(x=200, y=500)
 trainImg = tk.Button(window, text="Train ảnh", command=TrainImages  ,fg="steel blue"  ,bg="OliveDrab1"  ,width=20  ,height=3, activebackground = "white" ,font=('times', 15, ' bold '))
@@ -225,5 +225,5 @@ copyWrite.tag_configure("superscript", offset=10)
 copyWrite.configure(state="disabled",fg="white"  )
 copyWrite.pack(side="left")
 copyWrite.place(x=800, y=750)
- 
+
 window.mainloop()
